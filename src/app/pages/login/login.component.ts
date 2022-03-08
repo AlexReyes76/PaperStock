107c3b93ab3c353af4a5e29433e53928b2a70f03
AlexReyes76 <alex_dyk@outlook.es>
2022-03-07 22:26:47 -0600

    Funcionalidad de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  idInspector= new FormControl('',Validators.required);
+  password= new FormControl('',Validators.required);
+  verificar : String = '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
